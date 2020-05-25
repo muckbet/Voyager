0c169732268af0f958679f7bf68fdf74d9530621
Hryhoriy Ovsiannikov <ovsiannikov@ucu.edu.ua>
2020-05-25 15:56:03 +0300

    commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='Voyager',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    url='https://github.com/muckbet/Voyager',
+    license='MIT',
+    author='Hryhoriy Ovsiannikov',
+    author_email='ovsiannikov@ucu.edu.ua',
+    description='An educational app'
+)
